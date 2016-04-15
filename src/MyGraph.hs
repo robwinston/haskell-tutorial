3b2b193e7f0b99b03e825cc416a5ff7e1665d4be
Rob Winston <robtwinston@gmail.com>
2016-04-15 12:55:25 +0100

    Code clean-up

--- src/MyGraph.hs
+++ src/MyGraph.hs
@@ -2,7 +2,9 @@ module MyGraph where
 
 import Data.List (nub)
 
-
+-- functions below work with this graph definition
+-- a way to define graphs & functions to use any graph are a natural next step
+-- (when I know enough haskell to do it)
 edge :: Char -> Char -> Bool
 edge 'A' 'B' = True
 edge 'A' 'D' = True
@@ -15,46 +17,10 @@ edge 'F' 'E' = True
 edge _ _ = False
 
 
-connexions :: Char -> [Char]
-connexions a = filter (connected a) ['A'..'Z']
-
-connected :: Char -> Char -> Bool
-connected a b
-  | edge a b = True
-  | length possibleRoutes == 0 = False
-  | length (filter (== b) (map snd possibleRoutes)) == 0 = False
-  | otherwise = True
-    where possibleRoutes = routesFor (edgePairs a)
-
-
-route :: Char -> Char -> [(Char, Char)]
-route a b
-  | edge a b = [(a,b)]
-  -- next two patterns: if there's simply no way to get from a to b, don't bother tracing
-  | length possibleRoutes == 0 = []
-  | length (filter (== b) (map snd possibleRoutes)) == 0 = []
-  -- this is obviously not finished ...
-  | otherwise = possibleRoutes
-    where eps = edgePairs a
-          possibleRoutes = routesFor (eps)
-
-
-routesFor :: [(Char, Char)] ->  [(Char, Char)]
-routesFor rts
-  | length notTried == 0 = rts
-  | otherwise = routesFor (rts ++ concat (map edgePairs notTried))
-  where notTried = untried rts
-
-edges :: Char -> [Char]
-edges a = [e | e <- ['A'..'Z'], edge a e]
-
-edgePairs :: Char -> [(Char,Char)]
-edgePairs a = [(a,e) | e <- ['A'..'Z'], edge a e]
-
-allPathsFromTo :: Char ->  Char -> [[Char]]
+allPathsFromTo :: Char ->  Char -> [Path]
 allPathsFromTo f t = [np | np <- (allPathsFor f), last np == t]
 
-allPathsFor :: Char ->  [[Char]]
+allPathsFor :: Char ->  [Path]
 allPathsFor c = [np | np <- allPaths, head np == c]
 
 allPaths = [np | np <- buildPaths [a:[]| a <- ['A'..'F']], length np > 1]
@@ -68,7 +34,7 @@ buildPaths ps
   | otherwise = buildPaths (ps ++ (concat todo))
   where todo = map nextPaths (longestOnly ps)
 
-nextPaths :: [Char]-> [[Char]]
+nextPaths :: Path-> [[Char]]
 nextPaths [] = []
 nextPaths path
   | isCircular path = []
@@ -77,14 +43,9 @@ nextPaths path
   where todo = edges (last path)
 
 
-isCircular :: [Char] -> Bool
+isCircular :: Path -> Bool
 isCircular [c] = False
 isCircular p = head p == last p
-
--- given a char and a list of char pairs,
--- return all char pairs where char is first element
-nextSteps :: Char -> [(Char, Char)] -> [(Char, Char)]
-nextSteps s ps = filter (\p -> fst p == s) ps
 
 -- given a list of edge pairs,
 -- return all second elements which are not also first elements
@@ -92,10 +53,70 @@ nextSteps s ps = filter (\p -> fst p == s) ps
 untried :: [(Char, Char)] -> [Char]
 untried cps = filter (\u -> length (edges u) > 0) (nub [c | c <- map snd cps, not (elem c (map fst cps))])
 
-maxLength :: [[a]] -> Int
-maxLength l =  maximum $ map length l
+edges :: Char -> [Char]
+edges a = [e | e <- ['A'..'Z'], edge a e]
 
+-- helper function to add next path length to existing list of paths
+-- (because didn't know about 'iterate' yet)
 longestOnly :: [[a]] -> [[a]]
 longestOnly [[]] = [[]]
 longestOnly ps = [p | p <- ps, length p == maxLength ps]
+
+maxLength :: [[a]] -> Int
+maxLength l =  maximum $ map length l
+
+
+--- variation on what instructor did, using some of my solution ...
+
+type Path = [Char]
+
+allExtPathsFor :: Char -> [[Char]]
+allExtPathsFor c = [p | p <- allExtPaths, length p > 1 && head p == c]
+
+-- magic number related to number of vertices in example graph
+allExtPaths = concat $ take 7 extendPathss
+
+-- my 'iterate extendPaths' in effect starts over when it runs out of paths, because I proactively remove loops
+-- instructors solution lets it go on forever and worries about loops later
+-- that's probably more idiomatic, beacuse "start over"  behaviour is a bit odd
+-- for my approach, something like iterateWhile from https://hackage.haskell.org/package/monad-loops-0.4.3 would be useful here
+-- but not comfortable installing external packages yet ....
+extendPathss = iterate extendPaths []
+
+extendPaths :: [Path] -> [Path]
+extendPaths [] =  [[p] | p <- ['A'..'F']]
+extendPaths lp = concat $ map extendPath lp
+
+extendPath :: Path -> [Path]
+extendPath [] = []
+extendPath p
+  | length itsEdges == 0 = []
+  | otherwise = [p++[e] | e <- itsEdges, not $ isCircular p]
+  where itsEdges = edges (last p)
+
+-- inital noodling
+connexions :: Char -> [Char]
+connexions a = filter (connected a) ['A'..'Z']
+
+connected :: Char -> Char -> Bool
+connected a b
+  | edge a b = True
+  | length possibleRoutes == 0 = False
+  | length (filter (== b) (map snd possibleRoutes)) == 0 = False
+  | otherwise = True
+    where possibleRoutes = routesFor (edgePairs a)
+
+routesFor :: [(Char, Char)] ->  [(Char, Char)]
+routesFor rts
+  | length notTried == 0 = rts
+  | otherwise = routesFor (rts ++ concat (map edgePairs notTried))
+  where notTried = untried rts
+
+edgePairs :: Char -> [(Char,Char)]
+edgePairs a = [(a,e) | e <- ['A'..'Z'], edge a e]
+
+-- given a char and a list of char pairs,
+-- return all char pairs where char is first element
+nextSteps :: Char -> [(Char, Char)] -> [(Char, Char)]
+nextSteps s ps = filter (\p -> fst p == s) ps
 
