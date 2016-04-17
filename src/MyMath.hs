c6ad32fe0c9871e9897fbf35837a2efa61b193c5
Rob Winston <robtwinston@gmail.com>
2016-04-17 11:03:43 +0100

    Genericise functions

--- src/MyMath.hs
+++ src/MyMath.hs
@@ -7,44 +7,44 @@ factorial n
   | otherwise = factorial (-n)
 
 
-mygcd :: Int -> Int -> Int
+mygcd :: (Integral a) => a -> a -> a
 mygcd a 0 = a
 mygcd a b
   | a >= b = mygcd b (mod a b)
   | otherwise = mygcd b a
 
 
-isPrime :: Int -> Bool
+isPrime :: (Integral a) => a -> Bool
 isPrime x
   | x == 1 = False
   | otherwise = not (hasDivisor x (div x 2))
 
-hasDivisor :: Int -> Int -> Bool
+hasDivisor :: (Integral a) => a  -> a -> Bool
 hasDivisor x y
   | y < 2  = False
   | mod x y == 0 = True
   | otherwise = hasDivisor x (y-1)
 
 -- list comprehension basics
-divisors :: Int -> [Int]
+divisors :: (Integral a) => a  -> [a]
 divisors n
   | n < 0 = divisors (-n)
-  | otherwise = [x | x <- [1..((quotInt n 2)+1)], mod n x == 0]
+  | otherwise = [x | x <- [1..((quot n 2)+1)], mod n x == 0]
 
-quotInt dividend divisor  = fromIntegral (quot dividend divisor)
-
+primeDivisors :: (Integral a) => a  -> [a]
 primeDivisors n = [x | x <- divisors n, isPrime x ]
 
-primes :: [Int]
+primes :: (Integral a) => [a]
 primes = thisSieve [2..]
   where thisSieve (x:xs) = x:thisSieve [y| y <- xs, mod y x > 0]
 
 -- pythagorian triples
+pytriples :: (Integral a) => a -> [(a,a,a)]
 pytriples limit = [(x,y,z) | x <- [1..limit], y <- [(x+1)..limit], z <- [(y+1)..limit], x*x + y*y == z*z]
 
 
 -- prime factorization
-pfs :: Int -> [Int]
+pfs :: (Integral a) => a -> [a]
 pfs n
   | n < 0 = (-1) : pfs (abs n)
   | n == 0 = []
@@ -52,40 +52,40 @@ pfs n
   | otherwise = np : pfs (whatsLeft n np)
   where np = nextPf n
 
-nextPf :: Int -> Int
+nextPf :: (Integral a) => a -> a
 nextPf n
   | pds == [] = n
   | otherwise = head (pds)
   where pds = primeDivisors n
 
-whatsLeft :: Int -> Int -> Int
+whatsLeft :: (Integral a) => a -> a -> a
 whatsLeft n f
  | n == f = 0
- | otherwise = fromIntegral (quot n f)
+ | otherwise = quot n f
 
 
-nondecreasing :: [Int] -> Bool
+nondecreasing :: (Ord a) => [a] -> Bool
 nondecreasing xs = and [ x <= y |  (x,y) <- zip xs (tail xs) ]
 
-
+multiples :: (Integral a) => a -> [a]
 multiples n = [x | x <- [1..], mod x n == 0]
 
 -- insert value into an already sorted list
-insert :: Int -> [Int] -> [Int]
+insert :: (Ord a) => a -> [a] -> [a]
 insert y [] =  [y]
 insert y (x:xs)
   | y <= x = y:(x:xs)
   | otherwise = x:(insert y xs)
 
-isort :: [Int] -> [Int]
+isort :: (Ord a) => [a] -> [a]
 isort [] = []
 isort [a] = [a]
 isort (x:xs) = insert x (isort xs)
 
-isortf :: [Int] -> [Int]
+isortf :: (Ord a) => [a] -> [a]
 isortf l = foldr insert [] l
 
-bsort :: [Int] -> [Int]
+bsort :: (Ord a) => [a] -> [a]
 bsort [] = []
 bsort xs
  | length xs < 10 = isort xs
@@ -93,7 +93,7 @@ bsort xs
      where (leftHalf, rightHalf) = halveList xs
 
 
-qsort :: [Int] -> [Int]
+qsort :: (Ord a) => [a] -> [a]
 qsort [] = []
 qsort [a] = [a]
 qsort (pivot:xs) = (qsort lower) ++ (pivot : (qsort higher))
@@ -101,7 +101,7 @@ qsort (pivot:xs) = (qsort lower) ++ (pivot : (qsort higher))
         higher = [x| x <- xs, x > pivot]
 
 
-mergeSorted :: [Int] -> [Int] -> [Int]
+mergeSorted :: (Ord a) => [a] -> [a] -> [a]
 mergeSorted [] rs = rs
 mergeSorted ls [] = ls
 mergeSorted (l:ls) (r:rs)
