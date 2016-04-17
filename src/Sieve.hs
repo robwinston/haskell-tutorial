f6e62a17e9575728f6e8b2f317406fe6757b0148
Rob Winston <robtwinston@gmail.com>
2016-04-17 11:54:26 +0100

    Make Sieve functions Integral generic (& slower!)

--- src/Sieve.hs
+++ src/Sieve.hs
@@ -1,32 +1,33 @@
 module Sieve where
-sieve :: Int -> [Int]
+sieve :: (Integral a) => a -> [a]
 sieve n = sieveToUntil n (sieveSize n)
 
 -- find primes up to ceiling, stop if get limit
-sieveToUntil :: Int -> Int -> [Int]
+sieveToUntil :: (Integral a) => a -> a -> [a]
 sieveToUntil limit ceiling =  fst (factorOutShiftUntil limit ([], [2..ceiling]))
 
 -- estimate sieve size, from how many primes are asked for
-sieveSize :: Int -> Int
-sieveSize n = fromIntegral (truncate ((logBase 2 numN) * numN))
+sieveSize ::  (Integral a) => a -> a
+sieveSize n = truncate ((logBase 2 numN) * numN)
   where numN = fromIntegral n
 
 -- for 1st element in second list ->
 -- remove all of its multiples from this list
 -- append it to first list
-factorOutShiftUntil :: Int -> ([Int], [Int]) -> ([Int], [Int])
-factorOutShiftUntil limit xt
+factorOutShiftUntil :: (Integral a) => a -> ([a], [a]) -> ([a], [a])
+factorOutShiftUntil l xt
  | toDo == [] = xt
  | length done >= limit = xt
  | (head toDo) * (head toDo)  > last toDo = (done ++ take (limit - (length done)) toDo, [])
- | otherwise = factorOutShiftUntil limit (done ++ (fst xtNew), (snd xtNew))
+ | otherwise = factorOutShiftUntil l (done ++ (fst xtNew), (snd xtNew))
  where
+   limit = fromIntegral l
    done = fst xt
    toDo = snd xt
    nextOne = head toDo
    xtNew = ([nextOne], factorOut nextOne toDo)
 
 -- remove f and its multiples from l
-factorOut :: Int -> [Int] -> [Int]
+factorOut :: (Integral a) => a -> [a] -> [a]
 factorOut f l = [x | x <- l, mod x f /=0]
 
--- src/SieveSeq.hs
+++ src/SieveSeq.hs
@@ -4,29 +4,30 @@ import Sieve
 import Data.Sequence
 import Data.Foldable (toList)
 
-sieveSq :: Int -> [Int]
+sieveSq :: (Integral a) => a -> [a]
 sieveSq n = sieveToUntilSq n (sieveSize n)
 
 -- find primes up to ceiling, stop if get limit
-sieveToUntilSq :: Int -> Int -> [Int]
+sieveToUntilSq :: (Integral a) => a -> a -> [a]
 sieveToUntilSq limit ceiling =  fst (fosusl limit ([], [2..ceiling]))
 
 
 -- List wrapper for factorOutShiftUntilSeq
-fosusl :: Int -> ([Int], [Int]) -> ([Int], [Int])
+fosusl :: (Integral a) => a -> ([a], [a]) -> ([a], [a])
 fosusl limit xt =  toListTuple (factorOutShiftUntilSeq limit (fromListTuple xt))
 
 
 -- for 1st element in second list ->
 -- remove all of its multiples from this list
 -- append it to first list
-factorOutShiftUntilSeq :: Int -> (Seq Int, Seq Int) -> (Seq Int, Seq Int)
+factorOutShiftUntilSeq :: (Integral a) => a -> (Seq a, Seq a) -> (Seq a, Seq a)
 factorOutShiftUntilSeq limit xt
  | toDo == empty = xt
- | Data.Sequence.length done >= limit = xt
- | nextOne * nextOne  > lastOne = (done >< Data.Sequence.take (limit - (Data.Sequence.length done)) toDo, empty)
+ | Data.Sequence.length done >= limiti = xt
+ | nextOne * nextOne  > lastOne = (done >< Data.Sequence.take (limiti - (Data.Sequence.length done)) toDo, empty)
  | otherwise = factorOutShiftUntilSeq limit (done >< (fst xtNew), (snd xtNew))
  where
+   limiti = fromIntegral limit
    done = fst xt
    toDo = snd xt
    nextOne = index toDo 0
@@ -35,7 +36,7 @@ factorOutShiftUntilSeq limit xt
 
 
 -- remove f and its multiples from l
-factorOutSq :: Int -> Seq Int -> Seq Int
+factorOutSq :: (Integral a) => a -> Seq a -> Seq a
 -- factorOutSq f l = [x | x <- l, mod x f /=0]
 factorOutSq f l = Data.Sequence.filter (\x -> mod x f /=0) l
 
